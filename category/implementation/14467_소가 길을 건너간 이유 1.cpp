88728bcd994f19d478718eb7c17a2ce4f1a5957d
degurii <sansam709@gmail.com>
2017-12-01 22:34:04 +0900

    BOJ

---
+++ category/implementation/14467_소가 길을 건너간 이유 1.cpp
@@ -0,0 +1,38 @@
+/*
+문제
+이 관찰 기록을 가지고 소가 최소 몇 번 길을 건넜는지 알아보자. 즉 같은 번호의 소가 위치를 바꾼 것이 몇 번인지 세면 된다.
+
+입력
+첫 줄에 관찰 횟수 N이 주어진다. N은 100 이하의 양의 정수이다.
+다음 N줄에는 한 줄에 하나씩 관찰 결과가 주어진다. 관찰 결과는 소의 번호와 위치(0 또는 1)로 이루어져 있다.
+
+출력
+첫 줄에 소가 길을 건너간 최소 횟수를 출력한다.
+*/
+
+
+/*
+solve:
+그냥 구현하면 됨
+*/
+#include <iostream>
+using namespace std;
+#include <cstring>
+
+int n, p[11];
+int main() {
+	memset(p, -1, sizeof(p));
+	cin >> n;
+	int ans = 0;
+	for (int i = 0; i < n; i++) {
+		int u, v;
+		cin >> u >> v;
+		if (p[u] == -1)
+			p[u] = v;
+		else if(p[u] != v){
+			p[u] = v;
+			ans++;
+		}
+	}
+	cout << ans;
+}
---
+++ category/implementation/14469_소가 길을 건너간 이유 3.cpp
@@ -0,0 +1,37 @@
+/*
+문제
+모든 소가 농장에 입장하려면 얼마나 걸리는 지 구해보자.
+
+입력
+첫 줄에 100 이하의 양의 정수 N이 주어진다.
+다음 N줄에는 한 줄에 하나씩 소의 도착 시각과 검문 시간이 주어진다. 각각 1,000,000 이하의 양의 정수이다.
+
+출력
+모든 소가 농장에 입장하는 데 걸리는 최소 시간을 출력한다.
+*/
+
+
+
+/*
+solve:
+대충 구현한다
+*/
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n;
+pair<int, int> p[101];
+int main() {
+	cin >> n;
+	for (int i = 1; i < n + 1; i++) {
+		cin >> p[i].first >> p[i].second;
+	}
+	sort(p + 1, p + n + 1);
+	int t = 0;
+	for (int i = 1; i < n + 1; i++) {
+		if (t < p[i].first) t = p[i].first;
+		t += p[i].second;
+	}
+	cout << t;
+}
