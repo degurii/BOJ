5fb3f5f942635eddf91ae5552de4df7ffba5e6eb
degurii <sansam709@gmail.com>
2018-03-16 20:04:56 +0900

    Sieve of Eratosthenes

---
+++ base_code/Sieve-of-Eratosthenes/Sieve of Eratosthenes.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<bool> initPrime(int n) {
+	vector<bool> isPrime(n + 1, true);
+	isPrime[0] = isPrime[1] = false;
+	for (int i = 2; i < n + 1; i++) {
+		if (isPrime[i]) {
+			for (int j = i * 2; j < n + 1; j += i) {
+				isPrime[j] = false;
+			}
+		}
+	}
+	return isPrime;
+}
+int main() {
+	int n;
+	cin >> n;
+
+	// isPrime[x] : x가 소수이면 true
+	vector<bool> isPrime = initPrime(n);
+
+}
