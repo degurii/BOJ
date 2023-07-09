ae94656abf4230efaa988ae2e320a4f5d1319e0a
degurii.jeong <degurii.jeong@kakaocorp.com>
2023-07-09 20:16:46 +0900

    코사라주 js 수정

--- team_note/scc/kosaraju.js
+++ team_note/scc/kosaraju.js
@@ -1,7 +1,7 @@
 // deps
 const range = (e, b = 0) => Array(e).fill().map((_, i) => i + b);
 const array = v => (Array.isArray(v) ? v : [v]);
-const concatSafe = (target, source) => (Array.isArray(target) ? target.concat(source) : array(source));
+const concatSafe = (target, source) => (Array.isArray(target) ? target.concat(array(source)) : array(source));
 const size = v => {
   if (typeof v?.length === 'number') return v.length;
   else if (v && typeof v === 'object') return Object.keys(v).length;
